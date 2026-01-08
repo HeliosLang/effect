63d9d69d499f23ee4d3d908dc250a0e50fdfc8d9
Christian Schmitz <cschmitz398@gmail.com>
2026-01-08 17:45:05 -0600

    added networkParams

---
+++ src/Ledger/NetworkParams.ts
@@ -0,0 +1,42 @@
+import { Schema } from "effect"
+import * as TxOutputId from "./TxOutputId.js"
+
+/**
+ * The raw JSON can be downloaded from the following CDN locations:
+ *
+ *  - Preview: [https://network-status.helios-lang.io/preview/config](https://network-status.helios-lang.io/preview/config)
+ *  - Preprod: [https://network-status.helios-lang.io/preprod/config](https://network-status.helios-lang.io/preprod/config)
+ *  - Mainnet: [https://network-status.helios-lang.io/mainnet/config](https://network-status.helios-lang.io/mainnet/config)
+ *
+ * These JSONs are updated every 15 minutes.
+ *
+ * Only include the minimum fields needed. flattened so it can be extended more easily
+ *
+ * NetworkParams are a summary of the Era-specific params, relevant for tx building and validation
+ *
+ * Optionally, NetworkParams returned by a private node can specify a `collateralUTXO` to use (<txID>#<outputIndex> format). Any transaction submitted through that same node will then add the signature necessary to spend the collateral UTXO.
+ * This allows the collateral UTXO managed to be done in a central place (i.e. the node).
+ */
+export const NetworkParams = Schema.Struct({
+  txFeeFixed: Schema.Number,
+  txFeePerByte: Schema.Number,
+  exMemFeePerUnit: Schema.Number,
+  exCpuFeePerUnit: Schema.Number,
+  utxoDepositPerByte: Schema.Number,
+  refScriptsFeePerByte: Schema.Number,
+  collateralPercentage: Schema.Number,
+  maxCollateralInputs: Schema.Number,
+  maxTxExMem: Schema.Number,
+  maxTxExCpu: Schema.Number,
+  maxTxSize: Schema.Number,
+  secondsPerSlot: Schema.Number,
+  stakeAddrDeposit: Schema.Number,
+  refTopSlot: Schema.Number,
+  refTipTime: Schema.Number,
+  costModelParamsV1: Schema.Array(Schema.Number),
+  costModelParamsV2: Schema.Array(Schema.Number),
+  costModelParamsV3: Schema.Array(Schema.Number),
+  collateralUTXO: Schema.optional(TxOutputId.TxOutputId)
+})
+
+export type NetworkParams = Schema.Schema.Type<typeof NetworkParams>
