c706b407f66c9b38873008457de89edfedf999e5
Christian Schmitz <cschmitz398@gmail.com>
2026-06-09 10:58:27 -0600

    Add transaction query args

--- src/Cardano/Network/Txs.ts
+++ src/Cardano/Network/Txs.ts
@@ -3,15 +3,25 @@ import type { Address } from "../Ledger/Address.js"
 import type { TxHash } from "../Ledger/TxHash.js"
 import type { ConnectionError, UnexpectedFormat } from "./errors.js"
 
+export type TxsOrder = "asc" | "desc"
+
+export interface TxsArgs {
+  readonly address: Address
+  readonly count?: number
+  readonly fromBlock?: number
+  readonly order?: TxsOrder
+  readonly page?: number
+  readonly toBlock?: number
+}
+
 /**
  * A service that returns all transactions that spend from the given address, or pay to the given address
  *
- * TODO: add config for filtering, pagination etc. (these can potentially be a lot of transactions!)
  * TODO: add filtering by AssetClass
  */
 export class Txs extends Context.Tag("Cardano.Network.Txs")<
   Txs,
-  (args: { address: Address }) => Effect.Effect<
+  (args: TxsArgs) => Effect.Effect<
     {
       hash: TxHash
       indexInBlock?: number
